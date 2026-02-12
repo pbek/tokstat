a6615477e8e55e999f86671b56197c1ca8dd948d
Patrizio Bekerle <patrizio@bekerle.com>
2026-02-12 22:52:11 +0100

    devenv: add rust commit hooks
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,16 @@
 _:
 
 {
+
   # https://devenv.sh/languages/
   languages = {
     rust.enable = true;
+  };
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
   };
 
   # See full reference at https://devenv.sh/reference/options/
