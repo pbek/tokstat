eb68476cf4ff162343f809134790a49403d6c277
Patrizio Bekerle <patrizio@bekerle.com>
2026-03-03 21:09:15 +0100

    devenv: fix dev env for builds
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,13 @@
-_:
+{ pkgs, ... }:
 
 {
+  packages = with pkgs; [
+    openssl
+    pkg-config
+    dbus
+  ];
+
+  env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.dbus.dev}/lib/pkgconfig";
 
   enterShell = ''
     echo "🤖 Tokstat development environment"
